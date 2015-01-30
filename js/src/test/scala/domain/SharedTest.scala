260da0f7d19d0addbbc9b18a46503f342964b462
SemanticBeeng <nick@semanticbeeng.com>
2015-01-30 09:21:28 -0800

    wip: making uTests work
    Getting this
    
    (same issue)
    
    Seems js/test triggers another compile even though I did an explicit test:compile
    
    [jvm] $ clean
    [success] Total time: 0 s, completed Jan 30, 2015 9:09:42 AM
    [jvm] $ compile
    [info] Updating {file:/home/nickdsc/projects/play-scalajs-showcase/}js...
    [info] Resolving org.eclipse.jetty#jetty-continuation;8.1.16.v20140903 ...
    [info] Done updating.
    [info] Compiling 15 Scala sources to /home/nickdsc/projects/play-scalajs-showcase/js/target/scala-2.11/classes...
    [warn] /home/nickdsc/projects/play-scalajs-showcase/js/src/main/scala/example/ChatJS.scala:257: Members of traits, classes and objects extending js.Any may only contain members that call js.native. This will be enforced in 1.0.
    [warn]   val CONNECTING: Int = 0
    ..
    [info] Updating {file:/home/nickdsc/projects/play-scalajs-showcase/}jvm...
    [info] Resolving jline#jline;2.12 ...
    [info] Done updating.
    [info] Compiling 27 Scala sources and 1 Java source to /home/nickdsc/projects/play-scalajs-showcase/jvm/target/scala-2.11/classes...
    [warn] /home/nickdsc/projects/play-scalajs-showcase/jvm/app/models/TaskModel.scala:148: postfix operator * should be enabled
    
    [warn] one warning found
    [success] Total time: 47 s, completed Jan 30, 2015 9:10:48 AM
    [jvm] $ test:compile
    [info] Compiling 1 Scala source to /home/nickdsc/projects/play-scalajs-showcase/js/target/scala-2.11/test-classes...
    [info] Fast optimizing /home/nickdsc/projects/play-scalajs-showcase/jvm/target/scala-2.11/classes/public/javascripts/js-fastopt.js
    [info] Compiling 2 Scala sources to /home/nickdsc/projects/play-scalajs-showcase/jvm/target/scala-2.11/test-classes...
    [success] Total time: 17 s, completed Jan 30, 2015 9:11:13 AM
    
    [jvm] $ +js/test
    [info] Setting version to 2.10.4
    [info] Set current project to jvm (in build file:/home/nickdsc/projects/play-scalajs-showcase/)
    [info] Updating {file:/home/nickdsc/projects/play-scalajs-showcase/}js...
    [info] Resolving org.eclipse.jetty#jetty-continuation;8.1.16.v20140903 ...
    [info] Done updating.
    [info] Compiling 15 Scala sources to /home/nickdsc/projects/play-scalajs-showcase/js/target/scala-2.10/classes...
    [error] /home/nickdsc/projects/play-scalajs-showcase/js/src/main/scala/example/HangmanJS.scala:32: uPickle does not know how to read [shared.Hangman]s; define an implicit Reader[shared.Hangman] to teach it how
    [error]         game() = read[Hangman](r.responseText)
    [error]                               ^
    [error] /home/nickdsc/projects/play-scalajs-showcase/js/src/main/scala/example/HangmanJS.scala:38: uPickle does not know how to read [shared.Hangman]s; define an implicit Reader[shared.Hangman] to teach it how
    [error]           game() = read[Hangman](r.responseText)
    [error]                                 ^
    [error] /home/nickdsc/projects/play-scalajs-showcase/js/src/main/scala/example/HangmanJS.scala:47: uPickle does not know how to read [shared.Hangman]s; define an implicit Reader[shared.Hangman] to teach it how
    [error]           game() = read[Hangman](r.responseText)
    [error]                                 ^
    [error] /home/nickdsc/projects/play-scalajs-showcase/js/src/main/scala/example/TodoJS.scala:46: uPickle does not know how to read [Either[shared.domain.todo.Task,shared.domain.todo.TodoBusinessException]]s; define an implicit Reader[Either[shared.domain.todo.Task,shared.domain.todo.TodoBusinessException]] to teach it how
    [error]           read[Either[Task, TodoBusinessException]](r.responseText)
    [error]                                                    ^
    [error] /home/nickdsc/projects/play-scalajs-showcase/js/src/main/scala/example/TodoJS.scala:122: uPickle does not know how to read [List[shared.domain.todo.Task]]s; define an implicit Reader[List[shared.domain.todo.Task]] to teach it how
    [error]         read[List[Task]](r.responseText)
    [error]                         ^
    [error] 5 errors found
    [error] (js/compile:compile) Compilation failed
    [error] Total time: 5 s, completed Jan 30, 2015 9:11:32 AM

--- js/src/test/scala/domain/SharedTest.scala
+++ js/src/test/scala/domain/SharedTest.scala
@@ -1,6 +1,6 @@
 package domain
 
-import shared.domain.todo.{Plan, Task, TaskCreated}
+import shared.domain.todo._
 import utest._
 
 /**
@@ -11,12 +11,33 @@ object SharedTest extends TestSuite {
 
   def tests = TestSuite {
 
+    val todoApi:TodoIntf = null
+
     "A plan can have a task" - {
       val plan = new Plan
       plan.loadFromHistory(Seq(
-        TaskCreated(new Task(Some(1L), "Do this", false))
-      ))
+        TaskCreated(new Task(Some(1L), "Do this", false)),
+        TaskRedefined(1L, "Do this other thing"),
+        TaskCompleted(1L)))
+
+      todoApi.clearCompletedTasks
+
+      assert(plan.countLeftToComplete == 0)
+    }
+
+    "A plan can have two task" - {
+      val plan = new Plan
+      plan.loadFromHistory(Seq(
+        TaskCreated(new Task(Some(1L), "Do this", false)),
+        TaskRedefined(1L, "Do this other thing"),
+        TaskCompleted(2L),
+        TaskCreated(new Task(Some(2L), "Do this honey", false))))
+
+      todoApi.clearCompletedTasks
+
       assert(plan.countLeftToComplete == 1)
     }
   }
+
+
 }

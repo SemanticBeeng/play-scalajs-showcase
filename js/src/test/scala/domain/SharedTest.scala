cb80e501ad9b0fdabb3dd8170bfb28749aa6ab4f
SemanticBeeng <nick@semanticbeeng.com>
2015-02-01 06:53:47 -0800

    wip: evolving tests Refactoring business API away from CRUD towards functional
    Defect fix
    [jvm] $ +js/test
    [jvm] $ +jvm/test
    
    All work
    Cleanup

--- js/src/test/scala/domain/SharedTest.scala
+++ js/src/test/scala/domain/SharedTest.scala
@@ -15,11 +15,18 @@ object SharedTest extends TestSuite {
 
   def tests = TestSuite {
 
+    /**
+     * No need for explicit scoping like in Specs2 specs.
+     * Each test gets a fresh copy of these variables
+     */
     val taskPlan = new Plan
-    val todoApi:TaskManagement = new TodoServerMock()
+    val taskMgmt: TaskManagement = new TodoServerMock()
 
     "A plan can have a task" - {
-      
+
+      assert(taskPlan.size == 0)
+      assert(taskPlan.size == 0)
+
       taskPlan.loadFromHistory(Seq(
         TaskScheduled(new Task(Some(taskOne), "Do this")),
         TaskRedefined(taskOne, "Do this other thing"),
@@ -28,12 +35,14 @@ object SharedTest extends TestSuite {
       assert(taskPlan.size == 1)
       assert(taskPlan.countLeftToComplete == 0)
 
-      todoApi.clearCompletedTasks
+      taskMgmt.clearCompletedTasks
 
       assert(taskPlan.countLeftToComplete == 0)
     }
 
     "A plan can have two tasks" - {
+
+      assert(taskPlan.size == 0)
 
       taskPlan.loadFromHistory(Seq(
         TaskScheduled(new Task(Some(taskOne), "Do this")),
@@ -43,7 +52,7 @@ object SharedTest extends TestSuite {
 
       assert(taskPlan.size == 2)
 
-      todoApi.clearCompletedTasks
+      taskMgmt.clearCompletedTasks
 
       assert(taskPlan.countLeftToComplete == 1)
     }

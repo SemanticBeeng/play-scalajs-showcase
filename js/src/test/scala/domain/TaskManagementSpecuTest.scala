ca3ed2606bc05a5dd56aee18d2630f0038b08b44
SemanticBeeng <nick@semanticbeeng.com>
2015-02-06 15:31:13 -0800

    wip: getting uTest to run with Futures
    Migrated to ScalaJS 0.6.0
    utest-js-plugin no longer needed.
    
    See
    https://groups.google.com/d/msg/scala-js/Kzx9Iq3aTp0/5L5dxE4fMS0J
    
    https://groups.google.com/d/msg/scala-js/Kzx9Iq3aTp0/T8HfSxhlZVwJ
    
    This runs!!

--- js/src/test/scala/domain/TaskManagementSpecuTest.scala
+++ js/src/test/scala/domain/TaskManagementSpecuTest.scala
@@ -1,14 +1,15 @@
 package domain
 
 import shared.domain.TaskManagementModelProxy
+import shared.domain.todo._
 import utest._
 
 import scala.concurrent.Future
 
 /**
-* @todo Test async support in uTest
-*       https://github.com/lihaoyi/utest/issues/33#issuecomment-65055113
-*/
+ * @todo Test async support in uTest
+ *       https://github.com/lihaoyi/utest/issues/33#issuecomment-65055113
+ */
 
 object TaskManagementSpecuTest extends TestSuite /*with TaskManagementScope*/ {
 
@@ -22,39 +23,39 @@ object TaskManagementSpecuTest extends TestSuite /*with TaskManagementScope*/ {
      */
     val $ = new TaskManagementModelProxy()
 
-    //    "A plan can have a task" - {
-    //
-    //      assert($.taskPlan.isEmpty)
-    //
-    //      $.taskPlan.loadFromHistory(Seq(
-    //        TaskScheduled(Task($.taskOne, "Do this")),
-    //        TaskRedefined($.taskOne, "Do this other thing"),
-    //        TaskCompleted($.taskOne)))
-    //
-    //      assert($.taskPlan.size == 1)
-    //      assert($.taskPlan.countLeftToComplete == 0)
-    //
-    //      $.taskMgmt.clearCompletedTasks
-    //
-    //      assert($.taskPlan.countLeftToComplete == 0)
-    //    }
-    //
-    //    "A plan can have two tasks" - {
-    //
-    //      assert($.taskPlan.isEmpty)
-    //
-    //      $.taskPlan.loadFromHistory(Seq(
-    //        TaskScheduled(Task($.taskOne, "Do this")),
-    //        TaskRedefined($.taskOne, "Do this other thing"),
-    //        TaskScheduled(Task($.taskTwo, "Do this honey")),
-    //        TaskCompleted($.taskOne)))
-    //
-    //      assert($.taskPlan.size == 2)
-    //
-    //      $.taskMgmt.clearCompletedTasks
-    //
-    //      assert($.taskPlan.countLeftToComplete == 1)
-    //    }
+    "A plan can have a task" - {
+
+      assert($.taskPlan.isEmpty)
+
+      $.taskPlan.loadFromHistory(Seq(
+        TaskScheduled(Task($.taskOne, "Do this")),
+        TaskRedefined($.taskOne, "Do this other thing"),
+        TaskCompleted($.taskOne)))
+
+      assert($.taskPlan.size == 1)
+      assert($.taskPlan.countLeftToComplete == 0)
+
+      $.taskMgmt.clearCompletedTasks
+
+      assert($.taskPlan.countLeftToComplete == 0)
+    }
+
+    "A plan can have two tasks" - {
+
+      assert($.taskPlan.isEmpty)
+
+      $.taskPlan.loadFromHistory(Seq(
+        TaskScheduled(Task($.taskOne, "Do this")),
+        TaskRedefined($.taskOne, "Do this other thing"),
+        TaskScheduled(Task($.taskTwo, "Do this honey")),
+        TaskCompleted($.taskOne)))
+
+      assert($.taskPlan.size == 2)
+
+      $.taskMgmt.clearCompletedTasks
+
+      assert($.taskPlan.countLeftToComplete == 1)
+    }
 
     "fff" - {
       Future {
@@ -62,25 +63,20 @@ object TaskManagementSpecuTest extends TestSuite /*with TaskManagementScope*/ {
       }
     }
 
-    //    "A plan can have two tasks" - {
-    //
-    //      assert($.taskPlan.isEmpty)
-    //
-    //      $.do_scheduleNew("Do this") andThen { case r =>
-    //
-    //        val taskId = r.get.value
-    //        $.do_complete(taskId)
-    //
-    //      } andThen { case _ =>
-    //
-    //        $.do_clearCompletedTasks
-    //      }
-    //    }
+    "A plan can have two tasks" - {
+
+      assert($.taskPlan.isEmpty)
+
+      $.do_scheduleNew("Do this") andThen { case r =>
+
+        val taskId = r.get.value
+        $.do_complete(taskId)
+
+      } andThen { case _ =>
+
+        $.do_clearCompletedTasks
+      }
+    }
 
   }
-//
-//  tests.runAsync().onComplete {
-//    case Failure(ex) => println(ex); throw ex
-//    case Success(v) => assert(v == 1)
-//  }
 }
